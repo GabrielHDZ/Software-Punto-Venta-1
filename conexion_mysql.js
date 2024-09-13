179e4323a9c59a0983fa8afcdbd22dd1f290a3da
GabrielHDZ <gabrielhdz0102@gmail.com>
2024-09-13 12:02:40 -0600

    add .env and secret keys using with mysql conneccion

---
+++ conexion_mysql.js
@@ -0,0 +1,27 @@
+const mysql = require('mysql2/promise');
+require('dotenv').config();
+
+const password=process.env.DATABASE_MYSQL_KEY;
+const pool_mysql = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  password: password,
+  database: 'sakila'
+});
+module.export=pool_mysql;
+
+
+// Ejemplo de conexión:
+(async () => {
+  try {
+    const connection = await pool_mysql.getConnection();
+    console.log('Conexión a la base de datos establecida');
+    // Realizar consultas aquí
+    await connection.release();
+    // Ejecutar una consulta de selección
+  const [rows, fields] = await connection.execute('SELECT * FROM store');
+  console.log(rows); // Los resultados de la consulta
+  } catch (error) {
+    console.error('Error al conectar a la base de datos:', error);
+  }
+})();
